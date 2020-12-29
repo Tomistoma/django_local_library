6c9f51442691fb6542feffb4eff6eb608b1b2232
@Tomistoma <Tomas.libertatem@gmail.com>
2020-12-30 00:04:46 +0100

    update3 - '*' in setting

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1c7l7@-jvnyl7w+ni^&yn3s@4p@wep
 DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['polar-badlands-15079.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
