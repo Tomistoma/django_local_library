33b857f998ca214755d9e693b364444c917d8cfc
@Tomistoma <Tomas.libertatem@gmail.com>
2020-12-30 00:34:07 +0100

    u8

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x3!(t4x09i$x&uihe3!53jt&ocrv00
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
